3b793c873748f26a806f44826d083b255122643e
neutrogo <erbal98@gmail.com>
2022-12-25 15:42:13 +0300

    finished, added game logic and mild edge case assurance

--- script.js
+++ script.js
@@ -1,20 +1,82 @@
 function getComputerChoice() {
 
-    number = getRandomNo(1, 3);
+    let number = getRandomNo(1, 3);
 
     switch (number) {
         case 1:
-            console.log("Rock");
-            break;
+            return "rock";
         case 2:
-            console.log("Paper");
-            break;
+            return "paper";
         case 3:
-            console.log("Scissors");
-            break;
+            return "scissors";
     }
 }
 
 function getRandomNo(min, max) {
     return Math.floor(Math.random() * 3) + 1;
 }
+
+function getPlayerChoice() {
+    return prompt("Rock, Paper or Scissors?");
+}
+
+function getChoiceLogic(choice) {
+
+    switch (choice) {
+        case "rock":
+            return 1;
+        case "paper":
+            return 2;
+        case "scissors":
+            return 3;
+        default:
+            return 0;
+   }
+}
+
+function rockPaperScissors() {
+    let computerChoiceLogic = getChoiceLogic(getComputerChoice());
+    let playerChoiceLogic = getChoiceLogic(getPlayerChoice().toLowerCase());
+    gameLogic(playerChoiceLogic, computerChoiceLogic);
+}
+
+// potentially convert choice strings to numbers here? can insert the
+// strings directly into the win and loss strings
+function gameLogic(player, com) {
+    let choices = [player, com];
+    choices = choices.join(",");
+
+    // if the player input is invalid
+    if(player === 0) {
+        console.log("Invalid Input, please enter 'Rock', 'Paper', or 'Scissors'")
+    }
+    else if (player === com) {
+        console.log("Draw!")
+    }
+    else {
+        switch (choices) {
+            // player loses
+            case "1,2":
+                console.log("You Lose! Paper beats Rock");
+                break;
+            case "2,3":
+                console.log("You Lose! Scissors beat Paper");
+                break;
+            case "3,1":
+                console.log("You Lose! Rock beats Scissors");
+                break;
+            // player wins
+            case "2,1":
+                console.log("You Win! Paper beats Rock");
+                break;
+            case "3,2":
+                console.log("You Win! Scissors beat Paper");
+                break;
+            case "1,3":
+                console.log("You Win! Rock beats Scissors");
+                break;
+        }
+    }
+}
+
+rockPaperScissors();
